df2acfb07918eb45394b4e225a7ff6277f69335e
Julian Gerhards <96697201+juliangerhards@users.noreply.github.com>
2026-08-19 12:51:41 +0200

    version

--- console/nuxt.config.ts
+++ console/nuxt.config.ts
@@ -38,7 +38,7 @@ export default defineNuxtConfig({
     },
     runtimeConfig: {
         public: {
-            appVersion: "1.0.0-beta-13", // Updated by publish.sh
+            appVersion: "1.0.0-beta-14", // Updated by publish.sh
             apiBaseUrl: "http://localhost:8080",
             consoleBaseUrl: "http://localhost:3000",
             allowAccountCreation: false,
